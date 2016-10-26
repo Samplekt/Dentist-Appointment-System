89f292e3d3194a7d188fe39497818452806fd860
dukehm <dukehm@mail.lipscomb.edu>
2016-10-26 15:46:05 -0500

    Add files via upload

--- Time.java
+++ Time.java
@@ -1,12 +1,54 @@
-public class Time{
-  private int hour;
-  private int minute;
+import java.util.Scanner;
 
-  public void display(){
-    System.out.printf(hour+": %2d",minute);
-  }
+public class Time
+{
+	private int hours;
+	private int minutes;
+
+	public Time()
+	{
+		hours = 0;
+		minutes = 0;
+	}
+
+	public Time(int h,int m)
+	{
+		hours = h;
+		minutes = m;
+	}
+
+	public void display()
+	{
+		System.out.println("Time: " + hours + ":" + minutes);
+	}
+
+	public int addMinutes(int h, int m, int addedmin)
+	{
+		int total = m + addedmin;
+		if(total > 59)
+		 {
+		 	int result = total - 59;
+		 	h += 1;
+		 	m = result;
+		 }
+		return m;
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Would you like to add minutes (yes or no)? ");
+		String answer = sc.nextLine();
+		//System.out.println("/" + answer);
 
 
 
+		if (answer != "no")
+			{
+				System.out.print("How many minutes? ");
+				int numofmin = sc.nextInt();
+				m = addMinutes(hours,minutes,numofmin);
+			}
 
+	}
 }
