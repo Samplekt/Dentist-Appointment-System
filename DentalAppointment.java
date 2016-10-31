ac98b026f46396d59a11848b14526e2008f7a85f
dukehm <dukehm@mail.lipscomb.edu>
2016-10-31 18:32:29 -0500

    Add files via upload

--- DentalAppointment.java
+++ DentalAppointment.java
@@ -62,7 +62,7 @@ public class DentalAppointment
 
 
 	public static void main(String[] args){
-		for(int i = 0; i < 1; i++){
+		for(int i = 0; i < 3; i++){
 			Scanner sc = new Scanner(System.in);
 
 			System.out.println("Enter first name: ");
